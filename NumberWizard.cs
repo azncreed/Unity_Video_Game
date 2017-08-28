64b9213f0e63749e1464dfc82176b29c8fde4be4
Sylvia Shen <azncreedii@gmail.com>
2017-08-28 12:04:13 +1000

    initial commit

---
+++ NumberWizard.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+int max = 1000;
+int min = 1;
+int guess = 500;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Welcome To Number Wizard");
+		print("Pick a number, but don't tell me");
+
+		print("The highest number you can pick is " +max);
+		print("The lowest number you can pick is " +min);
+
+		print("Is the number higher or lower than " +guess);
+		print("Up = higher, down = lower, return/enter = equal");
+
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			print ("Up arrow was pressed");
+			min = guess;
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print ("Down arrow was pressed");
+		} else (Input.GetKeyDown (KeyCode.Return)) {
+			print("I won!");
+		}
+	} 
+}
+
+
+
